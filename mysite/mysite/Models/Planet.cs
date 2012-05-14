458c4f861a6f04c790a1c7a5abd57afba5564214
chavp <my.parinya@gmail.com>
2012-05-14 15:01:03 +0700

    update
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ mysite/mysite/Models/Planet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mysite.Models
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+    }
+}
